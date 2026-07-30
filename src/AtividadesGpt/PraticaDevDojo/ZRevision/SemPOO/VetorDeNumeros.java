2a53a7a119fd4c4ef7dd191f69d106943864f657
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-30 16:20:08 -0300

    Update VetorDeNumeros.java

--- src/AtividadesGpt/PraticaDevDojo/ZRevision/SemPOO/VetorDeNumeros.java
+++ src/AtividadesGpt/PraticaDevDojo/ZRevision/SemPOO/VetorDeNumeros.java
@@ -6,30 +6,27 @@ public class VetorDeNumeros {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] vertor = new int[5];
+        int[] vetor = new int[5];
 
-        System.out.println("Digite um valor para cada uma das posições: \n");
-        for (int i = 0; i < vertor.length; i++) {
+        System.out.println("Digite um valor para cada uma das posições:\n");
+        for (int i = 0; i < vetor.length; i++) {
             System.out.println("Posição n° " + (i + 1) + ": ");
-
-            int preencher = scanner.nextInt();
-            vertor[i] = preencher;
-
+            vetor[i] = scanner.nextInt();
         }
 
-        System.out.println(" ---- Valores digitados ---- " );
-        for (int j : vertor) {
+        System.out.println("---- Valores digitados ----");
+        for (int j : vetor) {
             System.out.print(j + ". ");
         }
+        System.out.println();
 
-//        outra opção que imaginei:
+        int soma = 0;
+        for (int k : vetor) {
+            soma += k;
+        }
 
-//        for (int j = 0; j < vertor.length; j++) {
-//            System.out.println("Posição n° " + (j + 1) + " = "  + vertor[j]);
-//        }
+        System.out.println("\nResultado da soma: " + soma);
 
-
-
-
+        scanner.close();
     }
 }
