ca96b13e36696be3bbcce79d45580e50615d2c0b
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-30 21:27:04 -0300

    Update Cliente.java

--- src/Faculdade/dominio/Cliente.java
+++ src/Faculdade/dominio/Cliente.java
@@ -5,4 +5,19 @@ public class Cliente {
     private String cpf;
 
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
