55dbb1a392baa08ba9f4763c9ace1e9adf304df6
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-30 21:27:20 -0300

    Update Modelo.java

--- src/Faculdade/dominio/Modelo.java
+++ src/Faculdade/dominio/Modelo.java
@@ -5,4 +5,19 @@ public class Modelo {
     private String nome;
 
 
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
