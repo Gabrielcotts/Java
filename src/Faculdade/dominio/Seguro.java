9a7af0ec2f9c0559b5e043a1bc2c405d31e4efce
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-30 21:27:35 -0300

    Update Seguro.java

--- src/Faculdade/dominio/Seguro.java
+++ src/Faculdade/dominio/Seguro.java
@@ -7,4 +7,35 @@ public class Seguro {
     private boolean vigencia;
 
 
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public boolean isVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(boolean vigencia) {
+        this.vigencia = vigencia;
+    }
 }
