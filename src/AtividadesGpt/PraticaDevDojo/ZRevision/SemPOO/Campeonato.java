ff6b0a0da1bb9f85a863375c514bc514b23805f4
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-08-13 13:21:24 -0300

    Update Campeonato.java

--- src/AtividadesGpt/PraticaDevDojo/ZRevision/SemPOO/Campeonato.java
+++ src/AtividadesGpt/PraticaDevDojo/ZRevision/SemPOO/Campeonato.java
@@ -10,12 +10,19 @@ public class Campeonato {
                 "Espanha"
         };
 
-        for (String time1 : times) {
+//        for (String time1 : times) {
+//            for (String time2 : times) {
+//                if (!time1.equals(time2)) {
+//                    System.out.println(time1 + " x " + time2);
+//                }
+//            }
+//        }
+        
+        for (int i = 0; i < times.length; i++) {
 
-            for (String time2 : times) {
-                if (!time1.equals(time2)){
-                    System.out.println(time1 + " x " + time2);
-                }
+            for (int j = i + 1; j < times.length; j++) {
+
+                System.out.println(times[i] + " x " + times[j]);
             }
         }
     }
