9e43c5035174f94d2079db12b193dcd18bac2657
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-08-13 21:27:16 -0300

    Add files via upload

---
+++ src/Faculdade/Matriz.java
@@ -0,0 +1,8 @@
+package Faculdade;
+
+public class Matriz {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2}, {3,4,5}};
+
+    }
+}
---
+++ src/Faculdade/TelaWelcome.java
@@ -0,0 +1,10 @@
+package Faculdade;
+
+import javax.swing.*;
+
+public class TelaWelcome {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome \nto \nJava\nPrograming!");
+        System.exit(0);
+    }
+}
