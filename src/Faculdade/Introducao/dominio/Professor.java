8ba43b37abcec11971c7102e401a3dd39acaaf09
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-08-20 21:52:26 -0300

    Add files via upload

---
+++ src/Faculdade/Introducao/dominio/Professor.java
@@ -0,0 +1,40 @@
+package Faculdade.dominio;
+
+public class Professor {
+    private String nome;
+    private int matricula;
+    private int cargaHoraria;
+
+    public Professor(String nome, int matricula, int cargaHoraria) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public Professor() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
