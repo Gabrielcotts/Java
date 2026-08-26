a380f4dce19f73c93943897acc300d3198807156
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-08-26 16:58:19 -0300

    Update Funcionario.java

--- src/DevDojo/JavaCore/Hheranca/dominio/Funcionario.java
+++ src/DevDojo/JavaCore/Hheranca/dominio/Funcionario.java
@@ -3,6 +3,23 @@ package DevDojo.JavaCore.Hheranca.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
+/*
+    Exemplo 01
+     public void imprimeDados(){
+        System.out.println(this.salario);
+     }
+
+    Exemplo 02
+        public void imprimeDados2(){
+        System.out.println(this.salario);
+    }
+ */
+    public void imprimeDados(){
+        super.imprimeDados();
+        System.out.println(this.salario);
+    }
+
+
     public double getSalario() {
         return salario;
     }
