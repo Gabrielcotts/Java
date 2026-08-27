6aecc3861ea7c6d36a88b43c09dcbb09cabab6e9
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-08-27 15:53:45 -0300

    Change access modifiers of Pessoa attributes to protected

--- src/DevDojo/JavaCore/Hheranca/dominio/Pessoa.java
+++ src/DevDojo/JavaCore/Hheranca/dominio/Pessoa.java
@@ -1,9 +1,9 @@
 package DevDojo.JavaCore.Hheranca.dominio;
 
 public class Pessoa {
-    private String nome;
-    private String cpf;
-    private Endereco endereco;
+    protected String nome;
+    protected String cpf;
+    protected Endereco endereco;
 
     public void imprimeDados() {
         System.out.println(this.nome);
