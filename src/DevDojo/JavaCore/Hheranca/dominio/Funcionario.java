aba0a16fe1f93760738543bc11a29a1e06f7f1ca
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-09 17:31:09 -0300

    Add constructors to Funcionario class

--- src/DevDojo/JavaCore/Hheranca/dominio/Funcionario.java
+++ src/DevDojo/JavaCore/Hheranca/dominio/Funcionario.java
@@ -3,17 +3,25 @@ package DevDojo.JavaCore.Hheranca.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
-/*
-    Exemplo 01
-     public void imprimeDados(){
-        System.out.println(this.salario);
-     }
-
-    Exemplo 02
-        public void imprimeDados2(){
-        System.out.println(this.salario);
+    public Funcionario(String nome){
+        super(nome);
     }
- */
+
+    public Funcionario(String nome, String cpf) {
+        super(nome, cpf);
+    }
+
+    /*
+        Exemplo 01
+         public void imprimeDados(){
+            System.out.println(this.salario);
+         }
+
+        Exemplo 02
+            public void imprimeDados2(){
+            System.out.println(this.salario);
+        }
+     */
     public void imprimeDados(){
         super.imprimeDados();
         System.out.println(this.salario);
