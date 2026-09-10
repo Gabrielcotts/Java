97f7b991a227d79a6751fa6431ab205cb166f72f
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-10 14:34:27 -0300

    Refactor Anime class initialization blocks

--- src/DevDojo/JavaCore/Fmodificadorestatico/dominio/Anime.java
+++ src/DevDojo/JavaCore/Fmodificadorestatico/dominio/Anime.java
@@ -7,7 +7,7 @@ public class Anime {
     private String nome;
 //  private int[] episodios = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     private static int[] episodios;
-    /*    0 - Bloco de inicialização é executado quando a JVM carregar a classe
+    /*    0 - Bloco de inicialização estático é executado quando a JVM carregar a classe
           1 - Alocado em memória para o objeto.
           2 - Cada atributo de classe é criado e inicializado com valores default ou o que for passado.
           3 - Bloco de inicialização é executado.
