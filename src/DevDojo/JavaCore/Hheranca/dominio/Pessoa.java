dc3bb2851f7000a74fd5c5ea8a43dc7ae0561a2f
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-10 14:58:55 -0300

    Update Pessoa.java

--- src/DevDojo/JavaCore/Hheranca/dominio/Pessoa.java
+++ src/DevDojo/JavaCore/Hheranca/dominio/Pessoa.java
@@ -5,13 +5,24 @@ public class Pessoa {
     protected String cpf;
     protected Endereco endereco;
 
+    static {
+        System.out.println("Dentro do bloco de inicialização estático de pessoa");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de pessoa 1");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de pessoa 2");
+    }
+
     public Pessoa(String nome){
+        System.out.println("Dentro do construtor de pessoa");
         this.nome = nome;
     }
-    public Pessoa(String nome, String cpf){
-        this(nome);
-        this.cpf = cpf;
-    }
+//    public Pessoa(String nome, String cpf){
+//        this(nome);
+//        this.cpf = cpf;
+//    }
 
     public void imprimeDados() {
         System.out.println(this.nome);
