9e7a7f6efdd0f77df25af7628e311bd2c628fa4e
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-10 14:58:39 -0300

    Enhance Funcionario with initialization blocks
    Added static and instance initialization blocks to Funcionario class.

--- src/DevDojo/JavaCore/Hheranca/dominio/Funcionario.java
+++ src/DevDojo/JavaCore/Hheranca/dominio/Funcionario.java
@@ -3,13 +3,24 @@ package DevDojo.JavaCore.Hheranca.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
-    public Funcionario(String nome){
-        super(nome);
+    static {
+        System.out.println("Dentro do bloco de inicialização estático de Funcionário");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de Funcionário 1");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização de Funcionário 2");
     }
 
-    public Funcionario(String nome, String cpf) {
-        super(nome, cpf);
+    public Funcionario(String nome){
+        super(nome);
+        System.out.println("Dentro do construtor de Funcionário");
     }
+
+//    public Funcionario(String nome, String cpf) {
+//        super(nome, cpf);
+//    }
 
     /*
         Exemplo 01
