de2eeef7f4f7347d8419e82778c35ce532ebf3fe
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-10 21:33:45 -0300

    Update AnimeTest01.java

--- src/DevDojo/JavaCore/Isobrescrita/test/AnimeTest01.java
+++ src/DevDojo/JavaCore/Isobrescrita/test/AnimeTest01.java
@@ -4,8 +4,11 @@ import DevDojo.JavaCore.Isobrescrita.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Code Geass");
-
-        System.out.println(anime);
+        Anime anime1 = new Anime("Code Geass");
+        Anime anime2 = new Anime("Death note");
+        
+        System.out.println(anime1);
+        System.out.println(anime2);
+        
     }
 }
