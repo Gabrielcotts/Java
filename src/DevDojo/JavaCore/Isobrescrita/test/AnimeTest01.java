0c1d4088249a20e1199c8d1616ebdffff5dcc426
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-10 21:11:11 -0300

    Add files via upload

---
+++ src/DevDojo/JavaCore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package DevDojo.JavaCore.Isobrescrita.test;
+
+import DevDojo.JavaCore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Code Geass");
+
+        System.out.println(anime);
+    }
+}
