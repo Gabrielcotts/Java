7adb131673b10bd004ca5ec02d5da07fc8bb1414
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-14 15:55:16 -0300

    Update CarroTest01.java

--- src/DevDojo/JavaCore/JmodificadorFinal/test/CarroTest01.java
+++ src/DevDojo/JavaCore/JmodificadorFinal/test/CarroTest01.java
@@ -1,13 +1,15 @@
 package DevDojo.JavaCore.JmodificadorFinal.test;
 
 import DevDojo.JavaCore.JmodificadorFinal.dominio.Carro;
+import DevDojo.JavaCore.JmodificadorFinal.dominio.Comprador;
 
 public class CarroTest01 {
     static void main(String[] args) {
         Carro carro = new Carro();
 //        System.out.println(carro.VELOCIDADE_LIMITE);
         System.out.println(Carro.VELOCIDADE_LIMITE);
-
-
+//        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Gabriel");
+        System.out.println(carro.COMPRADOR);
     }
 }
