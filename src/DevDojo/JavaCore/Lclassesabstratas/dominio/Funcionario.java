ba645060c1888432f88f6260cca2342b0809095a
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-22 17:11:09 -0300

    Update Funcionario.java

--- src/DevDojo/JavaCore/Lclassesabstratas/dominio/Funcionario.java
+++ src/DevDojo/JavaCore/Lclassesabstratas/dominio/Funcionario.java
@@ -4,8 +4,23 @@ public abstract class Funcionario {
     protected String nome;
     protected double salario;
 
+    /*Uma classe abstrata pode ter métodos concretos e abstratos,
+    mas uma classe concreta só pode ter métodoso concretos
+    e não pode possuir métodos abstratos*/
+
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        calcularBonus();
+    }
+
+    public abstract void calcularBonus();
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
     }
 }
