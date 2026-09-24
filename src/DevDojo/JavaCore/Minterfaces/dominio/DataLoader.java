45b7b7bcda3afb4692792e8c19cc9d6225295bff
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-09-24 16:05:58 -0300

    Update DataLoader.java

--- src/DevDojo/JavaCore/Minterfaces/dominio/DataLoader.java
+++ src/DevDojo/JavaCore/Minterfaces/dominio/DataLoader.java
@@ -1,9 +1,15 @@
 package DevDojo.JavaCore.Minterfaces.dominio;
 
 public interface DataLoader {
+    /*public static final*/ int MAX_DATA_SIZE = 10;
+
     /*public abstract*/ void carregar();
 
     /*public*/ default void checkPermission() {
         System.out.println("Fazendo checagem de permissões");
+    }
+
+     static void retrieveMaxDataSize(){
+        System.out.println("Dentro do retrieveMaxDataSize na interface");
     }
 }
